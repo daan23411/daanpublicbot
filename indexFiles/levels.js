1f21855ef9c64e94d15658d4afd612b840e392db
daan23411 <d.vrieling123@gmail.com>
2021-03-31 19:49:07 +0200

    Update levels.js

--- indexFiles/levels.js
+++ indexFiles/levels.js
@@ -2,7 +2,8 @@ const mongo = require('../mongo')
 const profileSchema = require('../schemas/profile-schema')
 
 module.exports = (client) => {
-
+  client.on('message', (message) => {
+    const { guild, member } = message
 
     addXP(guild.id, member.id, 23, message)
   })

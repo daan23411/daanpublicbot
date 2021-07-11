0bdba450983f10330a5448e25f911ac790ca52f4
daan23411 <d.vrieling123@gmail.com>
2021-07-11 20:02:15 +0200

    bug fixes

--- commands/setup/simjoin.js
+++ commands/setup/simjoin.js
@@ -2,8 +2,8 @@ module.exports = {
     requiredPermissions: [
         'ADMINISTRATOR'
     ],
-    category: 'Setup',
-    description: 'Simulate a join.',
+    Category: 'Setup',
+    Description: 'Simulate a join.',
     callback: async({ message, args, text, client}) => {
         client.emit('guildMemberAdd', message.member)
     }
